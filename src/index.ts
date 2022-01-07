0063536526eaae112e88d8694bca1e5ac8c7b3a3
Karthik VJ <karthikeyanvj@gmail.com>
2022-01-07 09:34:11 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import { interval } from 'rxjs';
+import * as PIXI from 'pixi.js';
+import { Model } from 'pixi3d';
 
-import { sum } from './example/add';
-import { HelloWorld } from './example/hello-world';
-
-const ob$ = interval(1000);
-ob$.subscribe(val => {
-	console.log(val);
+const app = new PIXI.Application({
+	width: 600,
+	height: 400,
+	backgroundColor: 0x1099bb,
+	resolution: window.devicePixelRatio || 1,
+	sharedTicker: true,
 });
 
-console.log(new HelloWorld());
-console.log(sum(40, 2));
+document.body.appendChild(app.view);
+
+PIXI.Loader.shared
+	.add('barramundi-fish', './resources/barramundi-fish.glb')
+	.load((_, resources) => {
+	  // this fails
+		// @ts-ignore
+    app.stage.addChild(Model.from(resources['barramundi-fish'].gltf));
+
+    // this works
+    // setTimeout(() => {
+    //   // @ts-ignore
+    //   app.stage.addChild(Model.from(resources['barramundi-fish'].gltf));
+    // }, 100);
+	});
